18fc98758cf2c73de62815498178a405c26b9120
Andrea Ros <a.ros@donboscosandona.it>
2026-04-15 12:22:11 +0200

    Ripristinata pagina welcome

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\NotaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [NotaController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
